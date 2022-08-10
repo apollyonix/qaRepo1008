e6e01b5a32e5cec3605b2ca71f83a358d8df86d2
Maxim Karpenko <mkarpenko@modeln.com>
2022-08-10 20:40:29 +0300

    First Test + maven project

---
+++ qaRepo1008/src/test/java/ua/hillel/tests/FirstTest.java
@@ -0,0 +1,39 @@
+package ua.hillel.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Maxim Karpenko mkarpenko@modeln.com
+ */
+
+public class FirstTest {
+  @Test
+  public void loginTest() {
+    WebDriverManager.chromedriver().setup();
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://the-internet.herokuapp.com/");
+
+    driver.findElement(By.linkText("Form Authentication")).click();
+
+    WebElement usernameInput = driver.findElement(By.cssSelector("#username"));
+    usernameInput.clear();
+    usernameInput.sendKeys("tomsmith");
+
+    WebElement passInput = driver.findElement(By.xpath("//input[@type='password']"));
+    passInput.clear();
+    passInput.sendKeys("SuperSecretPassword!");
+
+    driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+    String text = driver.findElement(By.tagName("h2")).getText();
+    Assert.assertEquals(text, "Secure Area", "User should be logged in");
+
+    driver.quit();
+  }
+}
